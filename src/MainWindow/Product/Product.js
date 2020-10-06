bb234f8d30d098132648525cdd8bf7e7f1671d0d
Filip Gatarić <filip1310@hotmail.com>
2020-10-06 17:58:15 +0200

    Add product table with mock data

--- src/MainWindow/Product/Product.js
+++ src/MainWindow/Product/Product.js
@@ -1,17 +1,62 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import 'antd/dist/antd.css';
+import { Table, Tag, Space, Button } from 'antd';
+
+const data = [];
+for (let i = 0; i < 100; i++) {
+    data.push({
+        id: i.toString(),
+        duljina: `${(10 * i) + 2}`,
+        sirina: `${10 * i}`,
+        visina: `${10 * i + 1}`,
+        naziv: `Proizvod ${i}`,
+    });
+}
+
+
+const columns = [
+    {
+        title: 'Šifra',
+        dataIndex: 'id',
+        id: 'id',
+    },
+    {
+        title: 'Naziv',
+        dataIndex: 'naziv',
+        id: 'naziv',
+    },
+    {
+        title: 'Duljina',
+        dataIndex: 'duljina',
+        id: 'duljina',
+    },
+    {
+        title: 'Širina',
+        dataIndex: 'sirina',
+        id: 'sirina',
+    },
+    {
+        title: 'Visina',
+        dataIndex: 'visina',
+        id: 'visina',
+    },
+    {
+        title: 'Akcije',
+        id: 'action',
+        render: (text, record) => (
+            <Space size="middle">
+                <a>Novi</a>
+                <a>Uredi</a>
+                <a>Briši</a>
+            </Space>
+        ),
+    },
+];
 
 
 function Product() {
-
-    useEffect(() => {
-
-    });
-
-
     return (
-        <div>
-            Ja sam Proizvod
-        </div>
+        <Table columns={columns} dataSource={data} />
     )
 }
 
