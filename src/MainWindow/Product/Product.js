d52cf833113286eb89de648c4ebe2b5a2ba03f46
Filip Gatarić <filip1310@hotmail.com>
2020-10-07 13:19:50 +0200

    Add delete and edit mock data in Product table

--- src/MainWindow/Product/Product.js
+++ src/MainWindow/Product/Product.js
@@ -1,26 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import 'antd/dist/antd.css';
-import { Table, Space, Button, Modal } from 'antd';
+import { Table, Space, Button, Modal, Popconfirm, message } from 'antd';
 import AddProduct from './AddProduct';
+import EditProduct from './EditProduct';
 
 const mockData = []
 
 for (let i = 0; i < 105; i++) {
     mockData.push({
         id: i.toString(),
+        naziv: `Proizvod ${i}`,
         duljina: `${(10 * i) + 2}`,
         sirina: `${10 * i}`,
         visina: `${10 * i + 1}`,
-        naziv: `Proizvod ${i}`,
     });
+}
+
+const classProduct = {
+    id: null,
+    naziv: "",
+    duljina: 0,
+    sirina: 0,
+    visina: 0,
 }
 
 function Product() {
 
     const [data, setData] = useState(mockData);
     const [visibleAddModal, setVisibleAddModal] = useState(false)
-    const [confirmLoading, setconfirmLoading] = useState(false)
-
+    const [visibleEditModal, setVisibleEditModal] = useState(false)
+    const [currentRow, setCurrentRow] = useState()
 
     useEffect(() => {
 
@@ -32,6 +41,7 @@ function Product() {
             title: 'Naziv',
             dataIndex: 'naziv',
             id: 'naziv',
+            fixed: 'left',
         },
         {
             title: 'Duljina',
@@ -51,40 +61,76 @@ function Product() {
         {
             title: 'Akcije',
             id: 'action',
-            render: (text, record) => (
-                <Space size="middle">
-                    <a>Uredi</a>
-                    <a>Briši</a>
-                </Space>
-            ),
+
+            render: (text, record) =>
+                data.length >= 1 ? (
+                    <Space size="middle">
+                        {setCurrentRow(record)}
+                        <a /**onClick={showEditModal()}*/>Uredi</a>
+                        <Popconfirm title="Jeste li sigurni da želite obrisati ovaj proizvod?"
+                            okText="Da"
+                            cancelText="Ne"
+                            onConfirm={() => handleDelete(record)}>
+                            <a>Obriši</a>
+                        </Popconfirm>
+                    </Space>
+                ) : null,
+
         },
     ];
 
-    const showModal = () => {
+    const showAddModal = () => {
         setVisibleAddModal(true)
     };
 
-    const hideModal = () =>{
+    const hideAddModal = () => {
         setVisibleAddModal(false)
     }
+
+    const showEditModal = (record) => {
+        console.log("klik")
+        setVisibleEditModal(true)
+    };
+
+    const hideEditModal = () => {
+        setVisibleEditModal(false)
+    }
+
+    const handleDelete = (record) => {
+        console.log(record)
+        setData(data.filter((item) => item.id !== record.id))
+
+        message.info('Obrisali ste odabrani proizvod');
+    };
+
 
     return (
         <div>
             <Button type="primary"
                 style={{ marginBottom: 15 }}
-                onClick={showModal}>
+                onClick={showAddModal}>
                 Dodaj
         </Button>
             <Modal
                 title="Dodaj novi proizvod"
                 visible={visibleAddModal}
-                onCancel={hideModal}
+                onCancel={hideAddModal}
                 footer={null}
             >
-                <AddProduct formFinish={hideModal}/>
+                <AddProduct formFinish={hideAddModal} />
+            </Modal>
+            <Modal
+                title="Uredi proizvod"
+                visible={visibleEditModal}
+                onCancel={hideEditModal}
+                footer={null}
+            >
+                <EditProduct formFinish={hideEditModal} row={currentRow} />
             </Modal>
             <Table columns={columns}
-                dataSource={data} />
+                dataSource={data}
+                scroll={{ x: 750, y: 550 }}
+            />
         </div>
     )
 }
